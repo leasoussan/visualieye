b2667593191788c05608dadd8f682dd034c037cd
Alex <alexander-replyansky@yandex.ru>
2023-08-25 16:53:04 +0300

    adding responsiveness and centering planner page

--- front_end/src/component/Home.js
+++ front_end/src/component/Home.js
@@ -12,13 +12,6 @@ export function Home() {
 
     return (
         <>
-          {/* <h1 className='h1tag'>I am H1</h1>
-          <h2>I am H2</h2>
-          <h3>I am H3</h3>
-          <h4>I am H4</h4>
-          <p>I am "P"</p> */}
-
-
             <div className='homePageDisplay'>
                 <img src={'https://www.oberlo.com/media/1608267092-life-goals.jpg?w=1824&fit=max'} style={{width:'60%', background:'cover'}} />
             </div>
